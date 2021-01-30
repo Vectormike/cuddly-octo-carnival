30acdf1ff9ee2629ac006cc0689316f60e5c8cf7
Vectormike <victorjonah199@gmial.com>
2021-01-30 22:47:26 +0100

    chore: change to mysql and refractor

--- config/database.js
+++ config/database.js
@@ -1,17 +1,20 @@
 const Sequelize = require('sequelize')
-const dotenv = require('dotenv')
 
-dotenv.config()
+// dotenv.config()
 
-module.exports = new Sequelize('sales', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
+// const pool = new Pool({
+//   user: 'me',
+//   host: 'localhost',
+//   database: 'api',
+//   password: 'password',
+//   port: 5432,
+// })
+const sequelize = new Sequelize('segwitz', 'root', 'root', {
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  port: 8889,
+  freezeTableName: true,
   operatorsAliases: false,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
 })
+
+module.exports = { sequelize }
